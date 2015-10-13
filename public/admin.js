49f71238562070f08a5aa6e0011dd330a36f9c05
nickells <ellsworth.nick@gmail.com>
2015-10-13 00:34:31 -0400

    changed npm start script

--- public/admin.js
+++ public/admin.js
@@ -1,14 +1,32 @@
 app.config(function($stateProvider){
 	$stateProvider.state('admin',{
 		url: '/admin',
-		templateUrl: 'views/admin.html',
-		controller: 'AdminCtrl'
+		templateUrl: 'views/admin.html'
+	})
+	$stateProvider.state('admin.newPost',{
+		url: '/new',
+		parent: 'admin',
+		templateUrl: 'views/admin.add-post.html',
+		controller: 'AddPostCtrl'
+	})
+	$stateProvider.state('admin.allPosts', {
+		url: '/all',
+		templateUrl: 'views/admin.all-posts.html',
+		controller: 'AllPostsCtrl',
+		resolve: {
+			BlogPosts: function(BlogFactory){
+				return BlogFactory.getBlogPosts
+			}
+		}
 	})
 })
 
 
-app.controller('AdminCtrl', function($scope,BlogFactory){
+app.controller('AllPostsCtrl', function($scope, BlogPosts){
+	$scope.posts = BlogPosts
+})
 
+app.controller('AddPostCtrl', function($scope,BlogFactory){
 	$scope.submit = function(post){
 		console.log('submitting')
 		return BlogFactory.addPost(post)
