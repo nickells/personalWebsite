797e1a87ca84603e43865d0d643f48dbd19cae54
Nick <newDrive@Nicks-MacBook-Pro.local>
2015-10-15 21:05:30 -0400

    syntax highlighting

--- public/app.js
+++ public/app.js
@@ -1,8 +1,17 @@
 var app = angular.module('Website', ["ui.router", "hc.marked"])
 
 app.config(['markedProvider', function(markedProvider) {
-      markedProvider.setOptions({gfm: true});
-    }])
+	markedProvider.setOptions({
+		gfm: true,
+		highlight: function (code, lang) {
+			if (lang) {
+				return hljs.highlight(lang, code, true).value;
+			} else {
+				return hljs.highlightAuto(code).value;
+			}
+		}
+	})
+}])
 
 
 
