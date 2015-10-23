ed8ec703525f4f2b1885e61f0c65adb8789f664c
nickells <ellsworth.nick@gmail.com>
2015-10-23 17:22:46 -0400

    redid projects page

--- public/projects.js
+++ public/projects.js
@@ -15,19 +15,19 @@ app.controller('ProjectsCtrl', function($scope,navService){
 		[{
 			name: "blipcrowd",
 			displayName: "BlipCrowd",
-			description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+			description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
 		}, {
 			name: "dictatone",
 			displayName: "Dictatone",
-			description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+			description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
 		}, {
 			name: "mementodash",
 			displayName: "MementoDash",
-			description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+			description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
 		}, {
 			name: "breezeblocks",
 			displayName: "Breezeblocks.io",
-			description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+			description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
 		}]
 
 	$scope.hi = "Greetings!"
